2515fac2904276b080ea7e3709762292aa23f35c
David Karn <david@simplii.net>
2023-05-26 18:46:14 -0500

    cleanup of scanned pages with ImageMagick and page-dewarp

---
+++ scan/download_utils.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+sudo apt install imagemagick
+pip install page-dewarp
+
--- scan/volumes/peter_lombard/scan.js
+++ scan/volumes/peter_lombard/scan.js
@@ -1,4 +1,19 @@
-const { exec, spawn } = require('node:child_process');
+const fs              = require('fs');
+const { exec, execSync, spawn } = require('node:child_process');
+
+function curry(that, ...args) {
+    return function(...calledWithArgs) {
+        const newArgs = [];
+        
+        for (const i in args) {
+            if (args[i] === undefined) 
+                newArgs.push(calledWithArgs.shift());
+
+            else 
+                newArgs.push(args[i]); }
+
+        const as = newArgs.concat(calledWithArgs);
+        return that.apply(that, as); }; }
 
 const sentences_urls = [
     "https://lib.undercaffeinated.xyz/get/PDF/5636/CalibreLibrary", // sentences 1
@@ -16,4 +31,35 @@ const convert_scans = () => {
         .map(
             file => exec("pdftoppm -r 300 -png " + file[0] + " " + file[1])); };
 
-convert_scans();
+const prep_files = (prefix, bg_color) => {
+    fs
+        .readdirSync('./')
+        .filter(fn => fn.slice(0, prefix.length) == prefix)
+        .map(curry(prep_file, bg_color)); };
+
+const prep_file = (bg_color, filename) => {
+    const exec = (str) => {
+        console.log(str);
+        execSync(str); };
+    
+    exec("convert -crop 50%x100% +repage -density 150 "
+         + filename + " "
+         + filename.replace('-','').replace(".png", "_%d.png"));
+
+    [filename.replace('-','').replace(".png", "_0.png"),
+     filename.replace('-','').replace(".png", "_1.png")]
+        .map(filename => {
+            const margin_fn = filename.replace(".png", "_margin.png");
+
+            // the margin on these scans is sometimes to slim, it needs padding
+            // in order for page-dewarp not to clip it
+            exec("convert " + filename + " -resize 110%x110% "
+                 + " -background '" + bg_color + "' "
+                 + " -gravity center -extent 110%x110% "
+                 + margin_fn);
+            exec("~/.local/bin/page-dewarp " + margin_fn);
+            exec("rm " + margin_fn + " " + filename); }); };
+
+//pull_scans();
+//convert_scans();
+prep_files('tomus3_-', "#CD974F");
